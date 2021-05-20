46cc5abe376e372bd6c4d416bdae387af439253d
Oleksandr Polishchuk <70407461+KSK86-Kim@users.noreply.github.com>
2021-05-20 17:36:34 +0300

    test-1

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component, Fragment } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
 //////
-import contats from './json/contacts.json';
+// import contats from './json/contacts.json';
 
 //////
 import Header from './components/Header';
@@ -18,7 +18,12 @@ import Filter from './components/Filter';
 
 class App extends Component {
     state = {
-        contacts: contats,
+        contacts: [
+            { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+            { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+            { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+            { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+        ],
         filter: '',
         name: '',
         number: '',
